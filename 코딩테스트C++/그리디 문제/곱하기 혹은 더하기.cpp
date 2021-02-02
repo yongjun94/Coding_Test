64eeb8d46f1e2ff05c7866bed2240cc61b31620d
yongjun94 <37098848+yongjun94@users.noreply.github.com>
2021-02-02 14:10:00 +0900

    Update 곱하기 혹은 더하기.cpp

--- 코딩테스트C++/그리디 문제/곱하기 혹은 더하기.cpp
+++ 코딩테스트C++/그리디 문제/곱하기 혹은 더하기.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+string S;
+int result;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> S;
+
+	for (int i = 0; i < S.size(); i++) {
+		int x;
+		x = S[i]-'0';
+
+		if (x==0 || x==1) {
+			result += x;
+		}
+		else {
+			if (result != 0) {
+				result *= x;
+			}
+			else {
+				result += x;
+			}
+		}
+	}
+
+	cout << result;
+
+}
