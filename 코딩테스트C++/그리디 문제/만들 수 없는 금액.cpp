60afad3e21689704d05a1081710410e640a084c8
yongjun94 <37098848+yongjun94@users.noreply.github.com>
2021-02-02 14:08:31 +0900

    Update 만들 수 없는 금액.cpp

--- 코딩테스트C++/그리디 문제/만들 수 없는 금액.cpp
+++ 코딩테스트C++/그리디 문제/만들 수 없는 금액.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int n;
+int coin[1000];
+int result;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> coin[i];
+	}
+
+	sort(coin, coin + n);
+
+	result = 1;
+
+	for (int i = 0; i < n; i++) {
+		if (result < coin[i]) {
+			break;
+		}
+		result += coin[i]; 
+	}
+	
+	cout << result;
+}
