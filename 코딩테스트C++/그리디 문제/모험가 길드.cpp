fd12c66c5e53da5230934b1dca064a7eeba182da
yongjun94 <37098848+yongjun94@users.noreply.github.com>
2021-02-02 14:10:52 +0900

    Update 모험가 길드.cpp

--- 코딩테스트C++/그리디 문제/모험가 길드.cpp
+++ 코딩테스트C++/그리디 문제/모험가 길드.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+vector<int> a;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		a.push_back(x);
+	}
+	
+	sort(a.begin(), a.end());
+
+	int group = 0;
+	int cnt = 0;
+
+	for (int i = 0; i < n; i++) {
+		cnt++;
+		if (cnt == a[i]) {
+			group++;
+			cnt = 0;
+		}
+	}
+	
+	cout << group;
+
+}
